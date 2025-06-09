66527038490a87b21903da9e213012904ce3a10f
damediop24 <142991900+damediop24@users.noreply.github.com>
2025-06-09 17:05:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", "24856774"))
+API_HASH = getenv("API_HASH", "c67513889c9d838ed41bf12155bc8bf3")
+BOT_TOKEN = getenv("BOT_TOKEN", "7229345973:AAFP67Ta3Z4CV66mC4SYL9Wjvf66b8S4sk4")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5118708665").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://damealorica:moitiere2A@saverbot.znrlqwc.mongodb.net/?retryWrites=true&w=majority&appName=SaverBot")
+LOG_GROUP = getenv("LOG_GROUP", "-1002707663831")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002707663831"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "100000"))
+WEBSITE_URL = getenv("WEBSITE_URL", "")
+AD_API = getenv("AD_API", "")
+STRING = getenv("STRING", "")
+YT_COOKIES = getenv("YT_COOKIES", "")
+INSTA_COOKIES = getenv("INSTA_COOKIES", "")
