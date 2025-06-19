09a8a620d4bbb62a85c80e91e534dfb71d3ca933
damediop24 <142991900+damediop24@users.noreply.github.com>
2025-06-19 17:35:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "24856774"))
 API_HASH = getenv("API_HASH", "c67513889c9d838ed41bf12155bc8bf3")
-BOT_TOKEN = getenv("BOT_TOKEN", "7373076497:AAFf3xvwQFXaM7gzmV-K1GzyceFCDf0FY4c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7542850907:AAELDDb7hV9D3SfLtlz5GE1CSL_LeGcRybk")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5118708665").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://damealorica:moitiere2A@saverbot.znrlqwc.mongodb.net/?retryWrites=true&w=majority&appName=SaverBot")
 LOG_GROUP = getenv("LOG_GROUP", "-1002707663831")
