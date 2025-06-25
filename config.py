05f44620fff74cb43096ec9aaf846939d05867a2
damediop24 <142991900+damediop24@users.noreply.github.com>
2025-06-25 14:40:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "24856774"))
-API_HASH = getenv("API_HASH", "c67513889c9d838ed41bf12155bc8bf3")
-BOT_TOKEN = getenv("BOT_TOKEN", "7542850907:AAELDDb7hV9D3SfLtlz5GE1CSL_LeGcRybk")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5118708665").split()))
+API_ID = int(getenv("API_ID", "24363932"))
+API_HASH = getenv("API_HASH", "d84176e864496c3cd2542c1a0de42c4a")
+BOT_TOKEN = getenv("BOT_TOKEN", "7920155198:AAEWhAaW3sNMfIZzILwjCYRMIifjRK3yqVQ")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8086008476").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://damealorica:moitiere2A@saverbot.znrlqwc.mongodb.net/?retryWrites=true&w=majority&appName=SaverBot")
 LOG_GROUP = getenv("LOG_GROUP", "-1002842488275")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002842488275"))
