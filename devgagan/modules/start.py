da40a4f4871d6d9ad87db6e2a12810a029406b7a
damediop24 <142991900+damediop24@users.noreply.github.com>
2025-06-27 01:35:12 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -174,7 +174,7 @@ async def terms(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/damed2025")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/dame2907")],
         ]
     )
     await message.reply_text(terms_text, reply_markup=buttons)
@@ -193,7 +193,7 @@ async def plan(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/damed2025")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/dame2907")],
         ]
     )
     await message.reply_text(plan_text, reply_markup=buttons)
@@ -212,7 +212,7 @@ async def see_plan(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/damed2025")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/dame2907")],
         ]
     )
     await callback_query.message.edit_text(plan_text, reply_markup=buttons)
@@ -230,7 +230,7 @@ async def see_terms(client, callback_query):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/damed2025")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/dame2907")],
         ]
     )
     await callback_query.message.edit_text(terms_text, reply_markup=buttons)
